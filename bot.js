3f70ff324d48ab371e258b0f4b32a9978b734f1c
Eric Tweedle <erictweeds@gmail.com>
2020-05-10 00:41:52 -0400

    added movie details to poll

--- bot.js
+++ bot.js
@@ -55,53 +55,63 @@ function autoPoll() {
 
 async function generatePoll(message) {
     var moviesChannel = bot.channels.cache.get("705311653113233444");
-    lots_of_messages_getter(moviesChannel).then(movies => {
-        let titles = [];
-        movies.forEach(movie => {
-            let valid = false;
-            let platform = "";
-            movie.reactions.cache.forEach(reaction => {
-                if (reaction._emoji.name == '😍') {
-                    valid = true;
-                    platform = "(N)";
-                } else if (reaction._emoji.name == '😆') {
-                    valid = true;
-                    platform = "(D+)";
-                }
-            });
-            if (valid) {
-                titles.push({
-                    title: movie.content,
-                    platform
-                });
+    let movies = await lots_of_messages_getter(moviesChannel);
+    let titles = [];
+    movies.forEach(movie => {
+        let valid = false;
+        let platform = "";
+        movie.reactions.cache.forEach(reaction => {
+            if (reaction._emoji.name == '😍') {
+                valid = true;
+                platform = "N";
+            } else if (reaction._emoji.name == '😆') {
+                valid = true;
+                platform = "D+";
             }
         });
-
-        //get 4 unique movies
-        let opt1 = 0, opt2 = 0, opt3 = 0, opt4 = 0;
-        while(opt1 == opt2 || opt1 == opt3 || opt1 == opt4 || opt2 == opt3 || opt2 == opt4 || opt3 == opt4) {
-            opt1 = Math.floor(Math.random()*titles.length);
-            opt2 = Math.floor(Math.random()*titles.length);
-            opt3 = Math.floor(Math.random()*titles.length);
-            opt4 = Math.floor(Math.random()*titles.length);
-        }
-
-        await getMovieDetails();
-        if (message) {
-            message.channel.send("Movies: \n" + ":regional_indicator_a: " + titles[opt1].title + " " + titles[opt1].platform +  "\n" 
-                + ":regional_indicator_b: " + titles[opt2].title + " " + titles[opt2].platform + "\n" 
-                + ":regional_indicator_c: " + titles[opt3].title + " " + titles[opt3].platform + "\n" 
-                + ":regional_indicator_d: " + titles[opt4].title + " " + titles[opt4].platform);
-        } else {
-            let general = bot.channels.cache.get("689660661923446864");
-            general.send("Movies: \n" + ":regional_indicator_a: " + titles[opt1].title + " " + titles[opt1].platform +  "\n" 
-            + ":regional_indicator_b: " + titles[opt2].title + " " + titles[opt2].platform + "\n" 
-            + ":regional_indicator_c: " + titles[opt3].title + " " + titles[opt3].platform + "\n" 
-            + ":regional_indicator_d: " + titles[opt4].title + " " + titles[opt4].platform);
+        if (valid) {
+            titles.push({
+                title: movie.content,
+                platform
+            });
         }
     });
+
+        //get 4 unique movies
+    let opt1 = 0, opt2 = 0, opt3 = 0, opt4 = 0;
+    while(opt1 == opt2 || opt1 == opt3 || opt1 == opt4 || opt2 == opt3 || opt2 == opt4 || opt3 == opt4) {
+        opt1 = Math.floor(Math.random()*titles.length);
+        opt2 = Math.floor(Math.random()*titles.length);
+        opt3 = Math.floor(Math.random()*titles.length);
+        opt4 = Math.floor(Math.random()*titles.length);
+    }
+    let movie1 = titles[opt1];
+    let movie2 = titles[opt2];
+    let movie3 = titles[opt3];
+    let movie4 = titles[opt4];
+
+    movie1.details = await getMovieDetails(movie1.title);
+    movie2.details = await getMovieDetails(movie2.title);
+    movie3.details = await getMovieDetails(movie3.title);
+    movie4.details = await getMovieDetails(movie4.title);
+
+    if (message) {
+        message.channel.send("Movies: \n" + ":regional_indicator_a: " + movie1.title + " | " + movie1.platform + " | " + movie1.details.Runtime + " | " + movie1.details.imdbRating +  "\n" 
+            + ":regional_indicator_b: " + movie2.title + " | " + movie2.platform + " | " + movie2.details.Runtime + " | " + movie2.details.imdbRating +  "\n" 
+            + ":regional_indicator_c: " + movie3.title + " | " + movie3.platform + " | " + movie3.details.Runtime + " | " + movie3.details.imdbRating +  "\n"
+            + ":regional_indicator_d: " + movie4.title + " | " + movie4.platform + " | " + movie4.details.Runtime + " | " + movie4.details.imdbRating +  "\n");
+    } else {
+        let general = bot.channels.cache.get("689660661923446864");
+        general.send("Movies: \n" + ":regional_indicator_a: " + movie1.title + " | " + movie1.platform + " | " + movie1.details.Runtime + " | " + movie1.details.imdbRating +  "\n" 
+        + ":regional_indicator_b: " + movie2.title + " | " + movie2.platform + " | " + movie2.details.Runtime + " | " + movie2.details.imdbRating +  "\n" 
+        + ":regional_indicator_c: " + movie3.title + " | " + movie3.platform + " | " + movie3.details.Runtime + " | " + movie3.details.imdbRating +  "\n"
+        + ":regional_indicator_d: " + movie4.title + " | " + movie4.platform + " | " + movie4.details.Runtime + " | " + movie4.details.imdbRating +  "\n");
+    }
 }
 
+async function getMovieDetails(title) {
+    return await fetch(`http://www.omdbapi.com/?t=${title}&apikey=${auth.OMDB_API_KEY}`).then(res => res.json());
+}
 
 
 //https://stackoverflow.com/questions/55153125/fetch-more-than-100-messages
